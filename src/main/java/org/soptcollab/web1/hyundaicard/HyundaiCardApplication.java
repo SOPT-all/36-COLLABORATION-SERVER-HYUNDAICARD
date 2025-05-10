a6c03bc52eacfe23e4977bb56a29978304207fa4
bingle625 <fhqhxm8899@gmail.com>
2025-05-10 14:58:12 +0900

    init

---
+++ src/main/java/org/soptcollab/web1/hyundaicard/HyundaiCardApplication.java
@@ -0,0 +1,13 @@
+package org.soptcollab.web1.hyundaicard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyundaiCardApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HyundaiCardApplication.class, args);
+  }
+
+}
---
+++ src/test/java/org/soptcollab/web1/hyundaicard/HyundaiCardApplicationTests.java
@@ -0,0 +1,13 @@
+package org.soptcollab.web1.hyundaicard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HyundaiCardApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
