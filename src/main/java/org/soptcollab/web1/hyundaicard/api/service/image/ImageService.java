19fde6daeb5957a51f7bb8d0d195b19b8a9746e8
rookie <piresj0318@gmail.com>
2025-05-14 16:35:01 +0900

    feat : 이미지 레포지토리 및 서비스 추가

---
+++ src/main/java/org/soptcollab/web1/hyundaicard/api/service/image/ImageService.java
@@ -0,0 +1,16 @@
+package org.soptcollab.web1.hyundaicard.api.service.image;
+
+import lombok.RequiredArgsConstructor;
+import org.soptcollab.web1.hyundaicard.Image.ImageRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ImageService {
+
+  private final ImageRepository imageRepository;
+
+
+
+
+}
