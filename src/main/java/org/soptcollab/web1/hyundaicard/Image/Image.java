485a577c4aeace0b7a023d837e67029058af4b1a
rookie <piresj0318@gmail.com>
2025-05-14 16:09:54 +0900

    feat : Image 엔티티 추가

---
+++ src/main/java/org/soptcollab/web1/hyundaicard/Image/Image.java
@@ -0,0 +1,38 @@
+package org.soptcollab.web1.hyundaicard.Image;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.soptcollab.web1.hyundaicard.global.common.entity.BaseEntity;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Image extends BaseEntity {
+
+  @Id @GeneratedValue(strategy = GenerationType.UUID)
+  @Column(name = "IMAGE_ID")
+  public String id;
+
+  // 이미지 url
+  public String url;
+
+  // 이미지 확장자
+  public String extension;
+
+  // 이미지 너비
+  public Integer width;
+
+  // 이미지 높이
+  public Integer height;
+
+
+
+
+
+}
