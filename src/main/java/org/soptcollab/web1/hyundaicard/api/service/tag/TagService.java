47933ef725ce53e04faa4fb07603734cc4ae83df
rookie <piresj0318@gmail.com>
2025-05-16 12:31:36 +0900

    refactor : 태그 리스트 API 의 반환 형식을 카테고리 기준으로 그룹핑

--- src/main/java/org/soptcollab/web1/hyundaicard/api/service/tag/TagService.java
+++ src/main/java/org/soptcollab/web1/hyundaicard/api/service/tag/TagService.java
@@ -1,8 +1,12 @@
 package org.soptcollab.web1.hyundaicard.api.service.tag;
 
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import org.soptcollab.web1.hyundaicard.api.service.card.dto.CardBrandGroupDto;
+import org.soptcollab.web1.hyundaicard.api.service.card.dto.CardResponseDto;
+import org.soptcollab.web1.hyundaicard.api.service.tag.dto.TagCategoryGroupDto;
 import org.soptcollab.web1.hyundaicard.api.service.tag.dto.TagResponseDto;
 import org.soptcollab.web1.hyundaicard.tag.TagRepository;
 import org.springframework.stereotype.Service;
@@ -13,10 +17,18 @@ public class TagService {
 
   private final TagRepository tagRepository;
 
-  public List<TagResponseDto> findAll() {
+  public List<TagCategoryGroupDto> findAll() {
 
-    return tagRepository.findAll().stream()
+    List<TagResponseDto> allTags = tagRepository.findAll().stream()
         .map(tag -> TagResponseDto.from(tag))
         .collect(Collectors.toList());
+
+    // Car
+    Map<String, List<TagResponseDto>> groupedByCategory = allTags.stream()
+        .collect(Collectors.groupingBy(TagResponseDto::category));
+
+    return groupedByCategory.entrySet().stream()
+        .map(entry -> new TagCategoryGroupDto(entry.getKey(), entry.getValue()))
+        .toList();
   }
 }
