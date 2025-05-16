7d82533ef0c3aefd06c9ff3ddef64ea9d573f341
bingle625 <fhqhxm8899@gmail.com>
2025-05-16 21:24:54 +0900

    feat: tag list dto 수정

--- src/main/java/org/soptcollab/web1/hyundaicard/api/service/tag/dto/TagResponseDto.java
+++ src/main/java/org/soptcollab/web1/hyundaicard/api/service/tag/dto/TagResponseDto.java
@@ -3,6 +3,7 @@ package org.soptcollab.web1.hyundaicard.api.service.tag.dto;
 import org.soptcollab.web1.hyundaicard.tag.Tag;
 
 public record TagResponseDto(
+    String tagId,
     String category,
     String name,
     String hoverText,
@@ -10,8 +11,13 @@ public record TagResponseDto(
 ) {
 
   public static TagResponseDto from(Tag t) {
-    return new TagResponseDto(t.getCategory().getType(),
-        t.getName(), t.getHoverText(), t.getDisplayOrder());
+    return new TagResponseDto(
+        t.getId(),
+        t.getCategory().getType(),
+        t.getName(),
+        t.getHoverText(),
+        t.getDisplayOrder()
+    );
   }
 
 }
